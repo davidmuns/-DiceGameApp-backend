579291e6411fc91a21786b57ad729496489f0fcf
David Gomez Muns <davidmuns@yahoo.es>
2022-09-01 23:20:24 +0200

    Adding crossOrigin anotation

--- src/main/java/com/juegodados/controllers/JuegoController.java
+++ src/main/java/com/juegodados/controllers/JuegoController.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,87 +22,88 @@ import com.juegodados.utilities.IUtilities;
 
 @RestController
 @RequestMapping("/juegodados/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class JuegoController {
-	
-	@Autowired
-	private IJuegoService iJuegoService;
 
-	@Autowired
-	private IJugadorService iJugadorService;
+    @Autowired
+    private IJuegoService iJuegoService;
 
-	@Autowired
-	private IUtilities iUtilities;
-	
-	// NUEVA JUGADA
-	@PostMapping("jugada/add/{idJugador}")
-	public ResponseEntity<?> jugar(@PathVariable("idJugador") int idJugador) {
+    @Autowired
+    private IJugadorService iJugadorService;
 
-		Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
+    @Autowired
+    private IUtilities iUtilities;
 
-		if (optionalJugador.isPresent()) {
-			Jugador jugador = optionalJugador.get();
-			
-			Juego nuevoJuego = iUtilities.getNuevoJuego(idJugador);
+    // NUEVA JUGADA
+    @PostMapping("jugada/add/{idJugador}")
+    public ResponseEntity<?> jugar(@PathVariable("idJugador") int idJugador) {
 
-			JuegoDto juegoDto = iJuegoService.guardarNuevoJuego(nuevoJuego, jugador);
-			
-			List<JuegoDto> juegosDto = iJuegoService.getAllJuegosJugador(jugador);
-			
-			Double ratioJugador = iUtilities.calcularRatioJugador(juegosDto);
-			
-			jugador.setRatio(ratioJugador);
-			
-			iJugadorService.guardarJugador(jugador);
-			
-			return ResponseEntity.ok(juegoDto);
-		}
+	Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
 
-		return ResponseEntity.status(404)
-				.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
+	if (optionalJugador.isPresent()) {
+	    Jugador jugador = optionalJugador.get();
+
+	    Juego nuevoJuego = iUtilities.getNuevoJuego(idJugador);
+
+	    JuegoDto juegoDto = iJuegoService.guardarNuevoJuego(nuevoJuego, jugador);
+
+	    List<JuegoDto> juegosDto = iJuegoService.getAllJuegosJugador(jugador);
+
+	    Double ratioJugador = iUtilities.calcularRatioJugador(juegosDto);
+
+	    jugador.setRatio(ratioJugador);
+
+	    iJugadorService.guardarJugador(jugador);
+
+	    return ResponseEntity.ok(juegoDto);
+	}
+
+	return ResponseEntity.status(404)
+		.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
+
+    }
+
+    // JUGADAS POR JUGADOR
+    @GetMapping("jugada/all/{idJugador}")
+    public ResponseEntity<?> getJugadasUnJugador(@PathVariable("idJugador") int idJugador) {
+
+	Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
+
+	if (optionalJugador.isPresent()) {
+
+	    Jugador jugador = optionalJugador.get();
+
+	    List<JuegoDto> juegosDto = iJuegoService.getAllJuegosJugador(jugador);
+
+	    return ResponseEntity.ok(juegosDto);
+	}
+	return ResponseEntity.status(404)
+		.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
+
+    }
+
+    // BORRAR TODAS LAS JUGADAS DE UN JUGADOR
+    @DeleteMapping("jugada/delete/all/{idJugador}")
+    public ResponseEntity<?> eliminarJugadasUnJugador(@PathVariable("idJugador") int idJugador) {
+
+	Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
+
+	if (optionalJugador.isPresent()) {
+	    Jugador jugador = optionalJugador.get();
+
+	    iJuegoService.eliminarAllJuegosJugador(jugador);
+
+	    jugador.setRatio(0.0);
+
+	    iJugadorService.guardarJugador(jugador);
+
+	    return ResponseEntity.ok(jugador);
+
+	} else {
+	    return ResponseEntity.status(404)
+		    .body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
 
 	}
-	
-	// JUGADAS POR JUGADOR
-	@GetMapping("jugada/all/{idJugador}")
-	public ResponseEntity<?> getJugadasUnJugador(@PathVariable("idJugador") int idJugador) {
 
-		Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
-
-		if (optionalJugador.isPresent()) {
-			
-			Jugador jugador = optionalJugador.get();
-			
-			List<JuegoDto> juegosDto = iJuegoService.getAllJuegosJugador(jugador);
-					
-			return ResponseEntity.ok(juegosDto);
-		}
-		return ResponseEntity.status(404)
-				.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
-
-	}
-	
-	// BORRAR TODAS LAS JUGADAS DE UN JUGADOR
-	@DeleteMapping("jugada/delete/all/{idJugador}")
-	public ResponseEntity<?> eliminarJugadasUnJugador(@PathVariable("idJugador") int idJugador) {
-
-		Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
-
-		if (optionalJugador.isPresent()) {
-			Jugador jugador = optionalJugador.get();
-				
-			iJuegoService.eliminarAllJuegosJugador(jugador);
-
-			jugador.setRatio(0.0);
-
-			iJugadorService.guardarJugador(jugador);
-			
-			return ResponseEntity.ok(jugador);
-
-		} else {
-			return ResponseEntity.status(404)
-					.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
-
-		}
-
-	}
+    }
 }
--- src/main/java/com/juegodados/controllers/JugadorController.java
+++ src/main/java/com/juegodados/controllers/JugadorController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.juegodados.dto.JugadorDto;
@@ -25,168 +25,167 @@ import com.juegodados.utilities.IUtilities;
 
 @RestController
 @RequestMapping(value = "/juegodados/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class JugadorController {
 
-	@Autowired
-	private IJuegoService iJuegoService;
+    @Autowired
+    private IJuegoService iJuegoService;
 
-	@Autowired
-	private IJugadorService iJugadorService;
+    @Autowired
+    private IJugadorService iJugadorService;
 
-	@Autowired
-	private IUtilities iUtilities;
+    @Autowired
+    private IUtilities iUtilities;
 
-	private DecimalFormat df = new DecimalFormat("####.##");
+    private DecimalFormat df = new DecimalFormat("####.##");
 
-	// OBTENER LISTADO DE TODOS LOS JUGADORES
-	@GetMapping(value = "/jugador/all")
-	public ResponseEntity<?> getJugadores() 
-	{
-		List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
-		
-		return ResponseEntity.ok(jugadoresDto);
-	}
+    // OBTENER LISTADO DE TODOS LOS JUGADORES
+    @GetMapping(value = "/jugador/all")
+    public ResponseEntity<?> getJugadores() {
+	List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
 
-	// CREAR NUEVO JUGADOR
-	@PostMapping(value = "/jugador/add")
-	public ResponseEntity<?> crearJugador(@RequestBody Jugador nuevoJugador) {
+	return ResponseEntity.ok(jugadoresDto);
+    }
 
-		List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
+    // CREAR NUEVO JUGADOR
+    @PostMapping(value = "/jugador/add")
+    public ResponseEntity<?> crearJugador(@RequestBody Jugador nuevoJugador) {
 
-		boolean existe = iUtilities.nombreJugadorExiste(jugadoresDto,
-				jugadorDto-> jugadorDto.getNombre().equalsIgnoreCase(nuevoJugador.getNombre())
-						&& !jugadorDto.getNombre().equalsIgnoreCase("anonimo"));
+	List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
 
-		
-		if (existe) { 
-		 
-		 return ResponseEntity
-				 .badRequest()
-				 .body("Ya existe un jugador llamado " + nuevoJugador.getNombre()); 
-		
-		}
-		 
-		
-		Jugador jugador = iUtilities.setAnonimoIfNameNullorBlank(nuevoJugador);
-		
-		JugadorDto jugadorDto = iJugadorService.guardarJugador(jugador);
+	boolean existe = iUtilities.nombreJugadorExiste(jugadoresDto,
+		jugadorDto -> jugadorDto.getNombre().equalsIgnoreCase(nuevoJugador.getNombre())
+			&& !jugadorDto.getNombre().equalsIgnoreCase("anonimo"));
 
-		return ResponseEntity.ok(jugadorDto);
-	}
+	if (existe) {
 
-	// MODIFICAR NOMBRE JUGADOR
-	@PutMapping(value = "/jugador/update/{idJugador}")
-	public ResponseEntity<?> modificarNombreJugador(@PathVariable("idJugador") int idJugador,
-			@RequestBody Jugador jugador) {
-
-		if (jugador.getNombre() == null)
-			jugador.setNombre("");
-
-		List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
-
-		boolean existe = iUtilities.nombreJugadorExiste(jugadoresDto,
-				jugadorDto -> jugadorDto.getNombre().equalsIgnoreCase(jugador.getNombre()));
-
-		Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
-
-		if (optionalJugador.isPresent() && existe == false && !jugador.getNombre().isBlank()) {
-
-			Jugador actualizarJugador = optionalJugador.get();
-			actualizarJugador.setNombre(jugador.getNombre());
-			
-			JugadorDto jugadorDto = iJugadorService.guardarJugador(actualizarJugador);
-			
-			return ResponseEntity.ok(jugadorDto);
-			//return ResponseEntity.ok("Jugador actualizado. Nombre nuevo: " + actualizarJugador.getNombre());
-		}
-
-		if (optionalJugador.isEmpty())
-			return ResponseEntity.status(404)
-					.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
-
-		if (existe)
-			return ResponseEntity.status(404).body(
-					"Ya existe un jugador llamado " + jugador.getNombre() + ". Consultar el listado de jugadores.");
-
-		if (jugador.getNombre().isBlank())
-			return ResponseEntity.status(404).body("El campo nombre es obligatorio.");
-
-		return null;
-	}
-
-	// ELIMINAR UN JUGADOR
-	@DeleteMapping("/jugador/delete/{idJugador}")
-	public ResponseEntity<?> eliminarUnJugador(@PathVariable("idJugador") int idJugador) {
-
-		Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
-
-		if (optionalJugador.isPresent()) {
-			iJugadorService.eliminarJugador(optionalJugador.get());
-			return ResponseEntity.ok("Eliminado jugador " + optionalJugador.get().getNombre() + " con id " + idJugador);
-		}
-		return ResponseEntity.status(404)
-				.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
-
-	}
-	// OBTENER PROCENTAJE TOTAL DE VICTORIAS
-	@GetMapping("/ratio-total-victorias")
-	public ResponseEntity<?> porcentajeVictorias() {
-
-		List<Juego> juegos = iJuegoService.getAllJuegos();
-
-		Double porcentajeTotalVictorias = iUtilities.getPorcentajeTotalVictorias(juegos);
-		
-		//return ResponseEntity.ok(porcentajeTotalVictorias);
-		return ResponseEntity.ok("Total partidas jugadas: " + juegos.size() + "\nTotal victorias: "
-				+ (int) ((juegos.size()) * (porcentajeTotalVictorias / 100)) + "\nPorcentaje victorias: "
-				+ df.format(porcentajeTotalVictorias) + " %");
+	    return ResponseEntity.badRequest().body("Ya existe un jugador llamado " + nuevoJugador.getNombre());
 
 	}
 
-	// OBTENER JUGADOR CON PEOR PORCENTAJE DE VICTORIAS
-	@GetMapping("ranking/peor")
-	public ResponseEntity<?> peorJugador() {
+	Jugador jugador = iUtilities.setAnonimoIfNameNullorBlank(nuevoJugador);
 
-		List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
+	JugadorDto jugadorDto = iJugadorService.guardarJugador(jugador);
 
-		JugadorDto peorJugadorDto;
+	return ResponseEntity.ok(jugadorDto);
+    }
 
-		if (jugadoresDto.size() != 0) {
-			peorJugadorDto = iUtilities.getRankingOrdenadoByRatio(jugadoresDto).get(jugadoresDto.size() - 1);
-			
-			return ResponseEntity.ok(peorJugadorDto);
-			//return ResponseEntity.ok("id: " + peorJugador.getIdJugador() + "\nNombre: " + peorJugador.getNombre()
-					//+ "\nRatio: " + df.format(peorJugador.getRatio()) + " %");
-		}
-		return ResponseEntity.status(404).body("No hay jugadores en la lista.");
+    // MODIFICAR NOMBRE JUGADOR
+    @PutMapping(value = "/jugador/update/{idJugador}")
+    public ResponseEntity<?> modificarNombreJugador(@PathVariable("idJugador") int idJugador,
+	    @RequestBody Jugador jugador) {
 
+	if (jugador.getNombre() == null)
+	    jugador.setNombre("");
+
+	List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
+
+	boolean existe = iUtilities.nombreJugadorExiste(jugadoresDto,
+		jugadorDto -> jugadorDto.getNombre().equalsIgnoreCase(jugador.getNombre()));
+
+	Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
+
+	if (optionalJugador.isPresent() && existe == false && !jugador.getNombre().isBlank()) {
+
+	    Jugador actualizarJugador = optionalJugador.get();
+	    actualizarJugador.setNombre(jugador.getNombre());
+
+	    JugadorDto jugadorDto = iJugadorService.guardarJugador(actualizarJugador);
+
+	    return ResponseEntity.ok(jugadorDto);
+	    // return ResponseEntity.ok("Jugador actualizado. Nombre nuevo: " +
+	    // actualizarJugador.getNombre());
 	}
 
-	// OBTENER JUGADOR CON MEJOR PORCENTAJE DE VICTORIAS
-	@GetMapping("ranking/mejor")
-	public ResponseEntity<?> mejorJugador() {
+	if (optionalJugador.isEmpty())
+	    return ResponseEntity.status(404)
+		    .body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
 
-		List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
+	if (existe)
+	    return ResponseEntity.status(404).body(
+		    "Ya existe un jugador llamado " + jugador.getNombre() + ". Consultar el listado de jugadores.");
 
-		JugadorDto mejorJugadorDto;
+	if (jugador.getNombre().isBlank())
+	    return ResponseEntity.status(404).body("El campo nombre es obligatorio.");
 
-		if (jugadoresDto.size() != 0) {
-			mejorJugadorDto = iUtilities.getRankingOrdenadoByRatio(jugadoresDto).get(0);
-			return ResponseEntity.ok(mejorJugadorDto);
-			//return ResponseEntity.ok("id: " + mejorJugador.getIdJugador() + "\nNombre: " + mejorJugador.getNombre()
-					//+ "\nRatio: " + df.format(mejorJugador.getRatio()) + " %");
-		}
-		return ResponseEntity.status(404).body("No hay jugadores en la lista.");
+	return null;
+    }
 
+    // ELIMINAR UN JUGADOR
+    @DeleteMapping("/jugador/delete/{idJugador}")
+    public ResponseEntity<?> eliminarUnJugador(@PathVariable("idJugador") int idJugador) {
+
+	Optional<Jugador> optionalJugador = iJugadorService.buscarPorIdJugador(idJugador);
+
+	if (optionalJugador.isPresent()) {
+	    iJugadorService.eliminarJugador(optionalJugador.get());
+	    return ResponseEntity.ok("Eliminado jugador " + optionalJugador.get().getNombre() + " con id " + idJugador);
 	}
-	
-	// OBTENER RANKING JUGADORES ORDENADO POR RATIO (MEJOR A PEOR)
-	@GetMapping("/ranking")
-	public ResponseEntity<?> getRankingOrdenado() {
-		List<JugadorDto> jugadoresDto = iUtilities
-				.getRankingOrdenadoByRatio(iJugadorService.getAllJugadores());
-		
-		return ResponseEntity.ok(jugadoresDto);
+	return ResponseEntity.status(404)
+		.body("El jugador con id " + idJugador + " no existe. Consultar el listado de jugadores.");
+
+    }
+
+    // OBTENER PROCENTAJE TOTAL DE VICTORIAS
+    @GetMapping("/ratio-total-victorias")
+    public ResponseEntity<?> porcentajeVictorias() {
+
+	List<Juego> juegos = iJuegoService.getAllJuegos();
+
+	Double porcentajeTotalVictorias = iUtilities.getPorcentajeTotalVictorias(juegos);
+
+	// return ResponseEntity.ok(porcentajeTotalVictorias);
+	return ResponseEntity.ok("Total partidas jugadas: " + juegos.size() + "\nTotal victorias: "
+		+ (int) ((juegos.size()) * (porcentajeTotalVictorias / 100)) + "\nPorcentaje victorias: "
+		+ df.format(porcentajeTotalVictorias) + " %");
+
+    }
+
+    // OBTENER JUGADOR CON PEOR PORCENTAJE DE VICTORIAS
+    @GetMapping("ranking/peor")
+    public ResponseEntity<?> peorJugador() {
+
+	List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
+
+	JugadorDto peorJugadorDto;
+
+	if (jugadoresDto.size() != 0) {
+	    peorJugadorDto = iUtilities.getRankingOrdenadoByRatio(jugadoresDto).get(jugadoresDto.size() - 1);
+
+	    return ResponseEntity.ok(peorJugadorDto);
+	    // return ResponseEntity.ok("id: " + peorJugador.getIdJugador() + "\nNombre: " +
+	    // peorJugador.getNombre()
+	    // + "\nRatio: " + df.format(peorJugador.getRatio()) + " %");
 	}
+	return ResponseEntity.status(404).body("No hay jugadores en la lista.");
+
+    }
+
+    // OBTENER JUGADOR CON MEJOR PORCENTAJE DE VICTORIAS
+    @GetMapping("ranking/mejor")
+    public ResponseEntity<?> mejorJugador() {
+
+	List<JugadorDto> jugadoresDto = iJugadorService.getAllJugadores();
+
+	JugadorDto mejorJugadorDto;
+
+	if (jugadoresDto.size() != 0) {
+	    mejorJugadorDto = iUtilities.getRankingOrdenadoByRatio(jugadoresDto).get(0);
+	    return ResponseEntity.ok(mejorJugadorDto);
+	    // return ResponseEntity.ok("id: " + mejorJugador.getIdJugador() + "\nNombre: "
+	    // + mejorJugador.getNombre()
+	    // + "\nRatio: " + df.format(mejorJugador.getRatio()) + " %");
+	}
+	return ResponseEntity.status(404).body("No hay jugadores en la lista.");
+
+    }
+
+    // OBTENER RANKING JUGADORES ORDENADO POR RATIO (MEJOR A PEOR)
+    @GetMapping("/ranking")
+    public ResponseEntity<?> getRankingOrdenado() {
+	List<JugadorDto> jugadoresDto = iUtilities.getRankingOrdenadoByRatio(iJugadorService.getAllJugadores());
+
+	return ResponseEntity.ok(jugadoresDto);
+    }
 
 }
